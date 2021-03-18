7207f860e0aac53a3f526f368db00f8cbbcff643
Dayanandh <dayanandhnagarajan@gmail.com>
2021-03-18 14:07:28 +0530

    deployment

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ldi@b03kd2_pho=--*@+*35zie2l0!&x!apsp=70p)edol#m*z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
